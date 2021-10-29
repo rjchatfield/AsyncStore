36f989c0b93984571b441bb244c88cc3c45049ff
Robert J Chatfield <rchatfield@atlassian.com>
2021-10-29 13:21:55 +1100

    Initial Commit

---
+++ AsyncStore/AsyncStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncStoreApp.swift
+//  AsyncStore
+//
+//  Created by Rob Chatfield on 29/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AsyncStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncStore
+//
+//  Created by Rob Chatfield on 29/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
