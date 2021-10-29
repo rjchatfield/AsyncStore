ab45e0d504a6629be9f11080e0a1830e1e6b7b21
Robert J Chatfield <rchatfield@atlassian.com>
2021-10-29 14:57:47 +1100

    async interleaving

--- AsyncArchitectureTests/AsyncStore.swift
+++ AsyncArchitectureTests/AsyncStore.swift
@@ -1,11 +1,5 @@
-//
-//  AsyncStore.swift
-//  AsyncStore
-//
-//  Created by Rob Chatfield on 29/10/21.
-//
-
 import Foundation
+import Combine
 
 struct State {
 }
@@ -51,11 +45,85 @@ enum Effect {
             return nil
         }
     }
+
+    func getReactions() -> ReactionSequence {
+        switch self {
+        case .subscribeToSomething:
+            return .publisher(
+                Timer.publish(every: 0.1, on: .main, in: .default)
+                    .autoconnect()
+                    .map { _ in .subscriptionTick }
+            )
+        case .retrieveSomething:
+            return .single {
+                .retrieveComplete
+            }
+        case .fetchSomething:
+            return .single {
+                do {
+                    print("😴 .fetchSomething (0.3s)")
+                    try await Task.sleep(nanoseconds: 300_000_000)
+                    print(" 😳 .fetchSomething (0.3s)")
+                    return .fetchComplete
+                } catch {
+                    return nil
+                }
+            }
+        case .saveSomething:
+            return .none
+        }
+
+    }
 }
 
-//struct ReactionSequence: AsyncSequence {
-//
-//}
+enum ReactionSequence: AsyncSequence {
+
+    case none
+    case single(() async -> Action?)
+    case stream((AsyncStream<Action>.Continuation) -> Void)
+
+    static func asyncSequence<Seq: AsyncSequence>(_ seq: Seq) -> Self where Seq.Element == Action {
+        .stream { continuation in
+            Task {
+                do {
+                    for try await value in seq {
+                        continuation.yield(value)
+                    }
+                } catch {
+                    // can't handle error. just finish.
+                }
+                continuation.finish()
+            }
+        }
+    }
+
+    static func publisher<P: Publisher>(_ p: P) -> Self where P.Output == Action, P.Failure == Never {
+        .asyncSequence(p.values)
+    }
+
+    typealias AsyncIterator = AsyncStream<Action>.AsyncIterator
+    typealias Element = Action
+
+    func makeAsyncIterator() -> AsyncStream<Action>.AsyncIterator {
+        AsyncStream { continuation in
+            switch self {
+            case .none:
+                continuation.finish()
+
+            case .single(let reactionBlock):
+                Task {
+                    if let reaction = await reactionBlock() {
+                        continuation.yield(reaction)
+                    }
+                    continuation.finish()
+                }
+
+            case .stream(let reactionsBlock):
+                reactionsBlock(continuation)
+            }
+        }.makeAsyncIterator()
+    }
+}
 
 /*
  TODO:
@@ -105,9 +173,14 @@ final class AsyncStore {
 //            Task(priority: .high) {
             Task {
                 print(effectCounter, "...", effect)
-                if let reaction = await effect() {
-                    print(effectCounter, " REACTION", reaction)
-                    await send(reaction, effectCounter + ".1")
+                var i = 1
+                for await reaction in effect.getReactions() {
+                    let reactionCounter = effectCounter + ".\(i)"
+                    i += 1
+                    print(reactionCounter, " REACTION", reaction)
+                    Task {
+                        await send(reaction, reactionCounter)
+                    }
                 }
             }
         }
